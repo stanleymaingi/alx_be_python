1504d0b17f0f4a3219ca16fa1b9b986b7794a930
stanleymaingi <stanleymaingi94@gmail.com>
2026-02-03 10:37:22 +0300

    Convert python_introduction to normal folder

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# basic_operations.py
+# Perform basic arithmetic operations without using global variables
+
+def main():
+    # Assign values to local variables
+    number1 = 10
+    number2 = 5
+
+    # Perform arithmetic operations and print results explicitly
+    print("Addition of", number1, "and", number2, "is", number1 + number2)
+    print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+    print("Multiplication of", number1, "and", number2, "is", number1 * number2)
+
